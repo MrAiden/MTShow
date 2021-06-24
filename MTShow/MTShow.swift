a960a25149d932dd552ea97728b578a63fb111f5
Mortar <yan_yan.vip@foxmail.com>
2021-06-24 15:45:21 +0800

    init commit

---
+++ MTShow/MTShow.swift
@@ -0,0 +1,23 @@
+//
+//  MTShow.swift
+//  Flats
+//
+//  Created by Mortar on 2020/4/1.
+//  Copyright © 2020 Mortar. All rights reserved.
+//  文字提示
+
+import Foundation
+
+public class MTShare {
+    
+    public func sharePrint() {
+        print("come from share....")
+    }
+    
+    public init() {}
+    
+    private func share() {
+        print("come from private share....")
+    }
+}
+
